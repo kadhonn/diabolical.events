44666e5c0cdcfe9271213ffde1a48a6ade28882b
abl <ableimat@gmx.at>
2025-03-09 22:04:09 +0100

    use real version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("events.boudicca:publisher-event-html:0.6.0-SNAPSHOT")
+	implementation("events.boudicca:publisher-event-html:0.6.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")

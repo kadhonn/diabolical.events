8e61d51428aabedf881f096382d0a56a8047ee28
abl <ableimat@gmx.at>
2025-03-09 14:39:58 +0100

    diabolical events v3

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "diabolical-events"
---
+++ src/main/kotlin/at/abl/diabolicalevents/Application.kt
@@ -0,0 +1,11 @@
+package at.abl.diabolicalevents
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
